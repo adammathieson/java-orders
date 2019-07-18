87ba37bfd801ecaf5f05d92ac5492a38f3bf4ac1
Adam Mathieson <mathieson1222@gmail.com>
2019-07-17 17:17:38 -0700

    Customers class added

--- src/main/java/com/lambdaschool/orders/model/Customers.java
+++ src/main/java/com/lambdaschool/orders/model/Customers.java
@@ -1,5 +1,187 @@
 package com.lambdaschool.orders.model;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
 public class Customers
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long customercode;
+
+    @Column(nullable = false)
+    private String custname;
+
+    private String custcity;
+    private String workinarea;
+    private String custcountry;
+    private String grade;
+    private double openingamt;
+    private double receiveamt;
+    private double paymentamt;
+    private double outstandingamt;
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "agentcode",
+                nullable = false)
+    private Agents agents;
+
+    @OneToMany(mappedBy = "customers",
+                cascade = CascadeType.ALL,
+                orphanRemoval = true)
+    private List<Orders> orders = new ArrayList<>();
+
+    public Customers(String custname)
+    {
+        this.custname = custname;
+    }
+
+    public Customers(String custname, String custcity, String workinarea, String custcountry, String grade, double openingamt, double receiveamt, double paymentamt, double outstandingamt, String phone, Agents agents)
+    {
+        this.custname = custname;
+        this.custcity = custcity;
+        this.workinarea = workinarea;
+        this.custcountry = custcountry;
+        this.grade = grade;
+        this.openingamt = openingamt;
+        this.receiveamt = receiveamt;
+        this.paymentamt = paymentamt;
+        this.outstandingamt = outstandingamt;
+        this.phone = phone;
+        this.agents = agents;
+    }
+
+    public long getCustomercode()
+    {
+        return customercode;
+    }
+
+    public void setCustomercode(long customercode)
+    {
+        this.customercode = customercode;
+    }
+
+    public String getCustname()
+    {
+        return custname;
+    }
+
+    public void setCustname(String custname)
+    {
+        this.custname = custname;
+    }
+
+    public String getCustcity()
+    {
+        return custcity;
+    }
+
+    public void setCustcity(String custcity)
+    {
+        this.custcity = custcity;
+    }
+
+    public String getWorkinarea()
+    {
+        return workinarea;
+    }
+
+    public void setWorkinarea(String workinarea)
+    {
+        this.workinarea = workinarea;
+    }
+
+    public String getCustcountry()
+    {
+        return custcountry;
+    }
+
+    public void setCustcountry(String custcountry)
+    {
+        this.custcountry = custcountry;
+    }
+
+    public String getGrade()
+    {
+        return grade;
+    }
+
+    public void setGrade(String grade)
+    {
+        this.grade = grade;
+    }
+
+    public double getOpeningamt()
+    {
+        return openingamt;
+    }
+
+    public void setOpeningamt(double openingamt)
+    {
+        this.openingamt = openingamt;
+    }
+
+    public double getReceiveamt()
+    {
+        return receiveamt;
+    }
+
+    public void setReceiveamt(double receiveamt)
+    {
+        this.receiveamt = receiveamt;
+    }
+
+    public double getPaymentamt()
+    {
+        return paymentamt;
+    }
+
+    public void setPaymentamt(double paymentamt)
+    {
+        this.paymentamt = paymentamt;
+    }
+
+    public double getOutstandingamt()
+    {
+        return outstandingamt;
+    }
+
+    public void setOutstandingamt(double outstandingamt)
+    {
+        this.outstandingamt = outstandingamt;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public Agents getAgents()
+    {
+        return agents;
+    }
+
+    public void setAgents(Agents agents)
+    {
+        this.agents = agents;
+    }
+
+    public List<Orders> getOrders()
+    {
+        return orders;
+    }
+
+    public void setOrders(List<Orders> orders)
+    {
+        this.orders = orders;
+    }
 }
